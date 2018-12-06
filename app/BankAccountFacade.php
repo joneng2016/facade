7edd98dcacea6f17660af95384e37540a1b72af3
root <root@debian.jonathan>
2018-12-06 16:55:45 -0200

    fazendo BankAccountFacade

--- app/BankAccountFacade.php
+++ app/BankAccountFacade.php
@@ -17,6 +17,36 @@ class BankAccountFacade
         $this->accountNumbert = $newAccNum;
         $this->securityCode = $newSecCode;
 
-        ...
+        $this->accChecker = new AccountNumberCheck();
+        $this->codeChecker = new SecurityCodeCheck();
+        $this->fundChecker = new FundsCheck(); 
+    }
+
+    public function getAccountNumber()
+    {
+        return $this->accountNumber;
+    }
+
+    public function getSecurityCode()
+    {
+        return $this->securityCode;
+    }
+    public function withdrawCash($cashToGet)
+    {
+        $isActive = $this->accChecker->accountActive($this->getAccountNumber());
+        $isCodeCorrect = $this->codeChecker->isCodeCorrect($this->getSecurityCode());
+        $hasEnoughMoney = $this->fundsChecker->haveEnoughMoney($cashToGet); 
+
+        if($isActive && $isCodeCorrect && $hasEnoughMoney)
+        {
+            echo "Saque: transação efetuada <br>"; 
+            return;
+        }
+        
+        echo "Saque: trasnacao não realizada<br>";
+    }
+    public function depositCash($cashToDeposit)
+    {
+    
     }
 }
